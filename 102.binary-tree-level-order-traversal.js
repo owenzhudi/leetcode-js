e97e5960ee727643a79a4cd7f10183524f125c92
Di Zhu <zhudi0928@gmail.com>
2019-06-11 19:27:21 -0700

    6/11/2019

---
+++ 102.binary-tree-level-order-traversal.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=102 lang=javascript
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if (!root) {
+    return [];
+  }
+  const queue = [];
+  const res = [];
+  queue.push(root);
+
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      level.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    res.push(level);
+  }
+  return res;
+    
+};
+
